1fe86b6222cbdb55eec8993485776c6753313b5c
JSandel12 <haythem.sandel@gmail.com>
2026-06-21 05:54:56 +0300

    chore: fix test limit env var in Run_VPN_Updater.bat

--- Run_VPN_Updater.bat
+++ Run_VPN_Updater.bat
@@ -3,6 +3,7 @@ echo ==============================================
 echo   Starting VPN Background Checker (1000 limit)
 echo ==============================================
 cd /d "%~dp0"
+set TEST_LIMIT=2000
 node index.js
 echo.
 echo Finished! Press any key to exit...
